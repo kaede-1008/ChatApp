f10e3ff936f27490de7c58f7555b4d61dda7152c
kaede_1008 <s16t203@stu.kagawa-u.ac.jp>
2020-05-23 22:08:58 +0900

    chat app成

---
+++ public/client.js
@@ -0,0 +1,28 @@
+const socket = io.connect();
+
+socket.on(
+    'const',
+    () => {
+        console.log('connect');
+    }
+);
+
+$ ('form').submit(
+    () => {
+        console.log('#input_message :', $('#input_message').val());
+        
+        if ( $('#input_message').val()) {
+            socket.emit('new message', $('#input_message').val());
+            $('#input_message').val('');
+        }
+        return false;
+    });
+
+socket.on(
+    'spread message',
+    (strMessage) => {
+        console.log('spread message: ', strMessage);
+
+        const li_element = $('<li>').text(strMessage);
+        $('#message_list').prepend(li_element);
+    });
---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// モジュール
+const express = require( 'express' );
+const http = require( 'http' );
+const socketIO = require( 'socket.io' );
+
+// オブジェクト
+const app = express();
+const server = http.Server( app );
+const io = socketIO( server );
+
+// 定数
+const PORT = process.env.PORT || 8080;
+
+// 接続時の処理
+// ・サーバーとクライアントの接続が確立すると、
+// 　サーバー側で、'connection'イベント
+// 　クライアント側で、'connect'イベントが発生する
+io.on(
+    'connection',
+    ( socket ) =>
+    {
+        console.log( 'connection' );
+
+        // 切断時の処理
+        // ・クライアントが切断したら、サーバー側では'disconnect'イベントが発生する
+        socket.on(
+            'disconnect',
+            () =>
+            {
+                console.log( 'disconnect' );
+            } );
+        socket.on(
+            'new message',
+            (strMessage) => {
+                console.log('new message', strMessage);
+
+                io.emit('spread message', strMessage);
+            });
+    } );
+
+// 公開フォルダの指定
+app.use( express.static( __dirname + '/public' ) );
+
+// サーバーの起動
+server.listen(
+    PORT,
+    () =>
+    {
+        console.log( 'Server on port %d', PORT );
+    } );
